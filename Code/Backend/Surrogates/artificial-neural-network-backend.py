51671f6ddd627e1850734a833c093b132860a3c6
Stean Kotze <stean@fastcomm.com>
2020-01-24 12:47:47 +0200

    [Frontend] Added frontend class for ANN and backend class for ANN

---
+++ Code/Backend/Surrogates/artificial-neural-network-backend.py
@@ -0,0 +1,40 @@
+class Backend_ANN:
+    def __init__(self, **kwargs) -> None:
+        return
+    
+    #   region Init
+
+    def __initialize_ann_geometry__(self, **kwargs) -> None:
+        return
+
+    def __initialize_ann_params__(self, **kwargs) -> None:
+        return
+
+    def __initialize_ann_weights__(self, **kwargs) -> None:
+        return
+
+    def __initialize_ann_nodes__(self, **kwargs) -> None:
+        return
+
+    #
+    #   endregion
+
+    #   region Sets
+
+    #
+    #   endregion
+
+    #   region Gets
+
+    #
+    #   endregion
+
+    #   region Training
+
+    #
+    #   endregion
+
+    #   region Other
+
+    #
+    #   endregion
---
+++ Code/Frontend/Surrogates/artificial-neural-network-frontend.py
@@ -0,0 +1,42 @@
+class Frontend_ANN:
+    def __init__(self, **kwargs) -> None:
+        return
+
+    #   region Imports-Exports
+
+    def import_ann_from_json(self, **kwargs) -> None:
+        return
+
+    def export_ann_from_json(self, **kwargs) -> None:
+        return
+
+    #
+    #   endregion
+
+    #   region Training
+
+    def train_ann_using_dataset(self, **kwargs) -> None:
+        return
+    
+    def propagate_data_sample_forward(self, **kwargs) -> None:
+        return
+
+    def propagate_error_backward(self, **kwargs) -> None:
+        return
+    
+    #
+    #   endregion
+
+    #   region Gets
+    
+    def get_ann_accuracy(self, **kwargs) -> None:
+        return
+
+    def get_ann_fitness(self, **kwargs) -> None:
+        return
+
+    def get_ann_error(self, **kwargs) -> None:
+        return
+
+    #
+    #   endregion
